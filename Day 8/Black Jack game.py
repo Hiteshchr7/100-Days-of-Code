844c0880e06f54ada1fc39ada7db0aa4dc255f08
Hitesh Chahar <hiteshchr7@gmail.com>
2025-06-24 12:23:23 +0530

    Updated the code
    Fully corrected the program

--- Day 8/Black Jack game.py
+++ Day 8/Black Jack game.py
@@ -1,77 +1,66 @@
 import random
+list_of_cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
 
 
+while True :
+    game = input("Do you want to play the game of blackjack? Type 'y' or 'n': ")
+    while game!= "y" and game != "n" :
+        print("Choose from 'y' or 'n' only. DON'T MAKE INVALID CHOICES") 
+        game = input("Do you want to play the game of blackjack? Type 'y' or 'n': ")
 
-def bust(l1):
-    return sum(l1) > 21
 
-# Case for Ace :
+    if game == "y" :
+        your_cards = random.choices(list_of_cards,k=2)
+        computer_cards = random.choices(list_of_cards,k=1)
+        print(f"your_cards: {your_cards} , current score: {sum(your_cards)} ")
+        print(f"Computer's first card: {computer_cards[0]}")
+        if sum(your_cards) == 21 :
+                print("Black Jack !!!")
+                print("You Win")
 
-def ace() :
-    if 11 in your_cards and your_score > 21 :
-        for card in your_cards :
-            if card == 11 :
-                card = 1
-        return list(your_cards)
-        
-    if 11 in computer_cards and sum(computer_cards) > 21 :
-        for card in computer_cards :
-            if card == 11 :
-                card = 1
-        return list(computer_cards)
-####################################################################################################################
-
-cards_list = [11,2,3,4,5,6,7,8,9,10,10,10,10,10]
-
-your_cards = list(random.choices(cards_list,k=2))
-your_score = sum(your_cards)
-
-computer_cards = list(random.choices(cards_list))
-
-print(f"Your Cards: {your_cards}, Current Score: {your_score}")
-print(f"Computer's first card: {computer_cards[0]}")
-
-if your_score == 21 :
-    print("Black Jack !!!")
-    print("You Win")
-
-else :
-
-    choose_next_card = input("Type 'y' to get another card, type 'n' to pass: ")
-    while choose_next_card not in ['y','n'] :
-        print("Your choice was not valid. Choose again from fiven inputs.")
-        choose_next_card = input("Type 'y' to get another card, type 'n' to pass: ")
-
-    if choose_next_card == 'n' :
-        computer_score = sum(computer_cards)
-
-        while computer_score <= 21 :
-            computer_cards.append(random.choice(cards_list))
-            ace()
-            print(computer_cards)
-            if  sum(computer_cards) > 21:
-                computer_cards.pop()
-                if sum(computer_cards) <=15 :
-                    computer_cards.append(random.choice(cards_list))
-                    ace()
-                    sum(computer_cards)
-                break
-
-            computer_score = sum(computer_cards)
-
-        
-        if sum(computer_cards) > 21 :
-            print(f"Your final hand: {your_cards}, Final Score: {your_score}")
-            print(f"Computer's final hand: {computer_cards}, Final Score: {sum(computer_cards)} ")
-            print("Your opponent went over. You Win")
         else :
-            print(f"Your final hand: {your_cards}, Final Score: {your_score}")
-            print(f"Computer's final hand: {computer_cards}, Final Score: {sum(computer_cards)} ")
-            if your_score > sum(computer_cards) :
-                print("Your opponent's score is less than yours. You Win")
-            elif your_score < sum(computer_cards) :
-                print("Your opponent's score is more than yours. You lose. ")
-            else :
-                print("It's a draw. ")
-    else:
-        pass
+            while sum(your_cards)<=21 :
+                choose_next_card = input("Type 'y' to get another card, type 'n' to pass: ")
+                while choose_next_card not in ['y','n'] :
+                    print("Your choice was not valid. Choose again from given inputs.")
+                    choose_next_card = input("Type 'y' to get another card, type 'n' to pass: ")
+                if choose_next_card == "n" :
+                    while sum(computer_cards) < 18 :
+                        computer_cards.append(random.choice(list_of_cards))
+                    while sum(computer_cards) > 21 and 11 in computer_cards:
+                        computer_cards[computer_cards.index(11)] = 1
+            
+                    print(f"Your final hand: {your_cards}, final score: {sum(your_cards)}")
+                    print(f"Computer's final hand: {computer_cards}, final score: {sum(computer_cards)}")
+                    if sum(computer_cards)>21 :
+                        print("Opponent went over. YOU WIN !!! ")   
+                    else:
+                        if sum(computer_cards) < sum(your_cards) :
+                            print("YOU WIN")
+                        elif sum(computer_cards) == sum(your_cards) :
+                            print("DRAW")
+                        else :
+                            print("YOU LOSE")
+                    break
+                        
+                            
+                else :
+                    your_cards.append(random.choice(list_of_cards))
+                    while sum(your_cards) >21 and 11 in your_cards :
+                        your_cards[your_cards.index(11)]=1
+                    print(f"your_cards: {your_cards} , current score: {sum(your_cards)} ")
+                    print(f"Computer's first card: {computer_cards[0]}")
+                    if sum(your_cards)>21 :
+                        print(f"Your final hand: {your_cards}, final score: {sum(your_cards)}")
+                        print(f"Computer's final hand: {computer_cards}, final score: {sum(computer_cards)}")
+                        print("You went over. YOU LOSE !!! ")
+                        break
+                        
+    play_again = input("Do you want to play another game? Type 'y' or 'n': ")
+    while play_again != "y" and play_again != "n":
+        print("Choose from 'y' or 'n' only.")
+        play_again = input("Do you want to play another game? Type 'y' or 'n': ")
+    if play_again == "n":
+        print("Thanks for playing. Goodbye!")
+        break
+        
