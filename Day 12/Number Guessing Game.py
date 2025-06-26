0a82aae74269ca811c35676994eba62a353879f1
Hitesh Chahar <hiteshchr7@gmail.com>
2025-06-26 23:04:44 +0530

    Add number guessing and prime check games
    Added 'Number Guessing Game.py', 'Prime.py', and 'art.py' under Day 12, implementing a number guessing game with difficulty selection and a simple prime number checker. Also removed the README.md file.

---
+++ Day 12/Number Guessing Game.py
@@ -0,0 +1,35 @@
+import random
+from art import logo
+print(logo)
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+diff = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+while diff not in ["easy", "hard"]:
+    print("Choose a valid response.")
+    diff = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+
+num =  random.randint(1,100)
+if diff == "easy" :
+    no_of_attempts = 10
+else :
+    no_of_attempts =  5
+        
+for i in range(no_of_attempts+1):
+    if no_of_attempts-i == 0 :
+        print("You have run out of guesses. YOU LOSE ")
+        break
+        
+    print(f"You have {no_of_attempts-i} attempts remaining to guess the number.")
+    guess = int(input("Make a guess: "))
+
+    if guess == num :
+        print(f"You got it. The answer was {guess}. YOU WIN!")
+        break
+    elif guess < num :
+        print("Too low.")
+        print("Guess again.\n")
+    else :
+        print("Too high.")
+        print("Guess again.\n")
+
+
